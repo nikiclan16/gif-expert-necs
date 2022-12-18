f6f2d3c649143d9e736a0f0626e5d0dc32f77c32
Vladimir <vladistar200304@gmail.com>
2022-12-18 16:23:33 -0500

    primer comit

---
+++ tests/components/GifItem.test.jsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import { GifItem } from "../../src/components/GifItem";
+
+
+
+describe('Pruebas en GifItem ', () => {
+    
+    const title = 'saitama';
+    const url = 'https://one-punch.com/saitama.jpg';
+
+    test('debe de hacer match con el snapshot', () => {
+        
+        const {container} = render(<GifItem title={title} url={url} />);
+        expect(container).toMatchSnapshot();
+
+    });
+    
+
+
+    test('debe de mostrar la imagen con el URL y el ALT indicado', () => { 
+
+        render(<GifItem title={title} url={url}/>);
+        // expect(screen.getByRole('img').alt).toBe(title);
+        // expect(screen.getByRole('img').src).toBe(url);
+        const {alt, src} = screen.getByRole('img');
+        expect(alt).toBe(title);
+        expect(src).toBe(url);
+
+     });
+
+     test('debe de mostrar el titulo en el componente', () => {
+
+        render(<GifItem title={title} url={url} /> );
+        expect( screen.getByText(title) ).toBeTruthy();
+    
+    });
+
+    
+});
